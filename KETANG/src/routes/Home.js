1806a560f41039d7c1c00e2d34e8af68dd66d94e
lijingliang1215 <1205175447@qq.com>
2018-07-03 16:20:59 +0800

    公共部分修改

--- KETANG/src/routes/Home.js
+++ KETANG/src/routes/Home.js
@@ -1,21 +1,27 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux'
-import NavTop from '../component/NavTop'
+import {Icon,Input} from 'antd';
+import '../static/css/home.less'
 
 class Home extends Component{
     constructor(props,context){
         super(props,context);
-        this.state={
-            title:'首页'
-        }
     }
     render(){
-        return (
-            <div>
-                <NavTop title={this.state.title}/>
+        return <div>
+                <header className='headerNavBox'>
+                    <div className='homeBox'>
+                        <div className='baseBox'>
+                            <h1 className='logo'>当当</h1>
+                            <div>
+                                <Input  placeholder="百丽童鞋" prefix={<Icon type='search'/>}  className='inputBox' />
+                            </div>
+                            <Icon className='icon' type='menu-fold'/>
+                        </div>
+                    </div>
+                </header>
                 <main className='allBox'>home</main>
             </div>
-        )
     }
 }
 export default connect()(Home)
