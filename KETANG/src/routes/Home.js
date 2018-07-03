5c85cf734bd8093e1ec590882459bf489536a8c0
fuyao121701 <m13739732328@163.com>
2018-07-03 17:05:57 +0800

    首页

--- KETANG/src/routes/Home.js
+++ KETANG/src/routes/Home.js
@@ -1,13 +1,9 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import NavTop from '../component/NavTop'
 import { Carousel } from 'antd';
-
 import {Icon,Input} from 'antd';
 import '../static/css/home.less'
-
-
 class Home extends Component {
     constructor(props, context) {
         super(props, context);
@@ -15,8 +11,6 @@ class Home extends Component {
             title: '首页'
         }
     }
-
-
 
     render(){
         return <div>
@@ -31,15 +25,12 @@ class Home extends Component {
                         </div>
                     </div>
                 </header>
-                <main className='allBox'><main className='allBox'>
-                <Carousel autoplay>
-                <div><img src="http://img55.ddimg.cn/82310034954145_y.jpg" alt=""/></div>
-                <div><img src="http://img63.ddimg.cn/upload_img/00702/B/1242x366_dl_20180629-1530512215.jpg" alt=""/></div>
-                <div><img src="http://img61.ddimg.cn/upload_img/00670/qd/rhgu79kuo-1530528732.jpg" alt=""/></div>
-                <div><img src="http://img60.ddimg.cn/upload_img/00609/mao/b0628banner1242366-1530524637.jpg" alt=""/></div>
-                </Carousel>
-                </main></main>
-            </div>
+
+                <main className='allBox'>
+                   home
+                </main>
+        </div>
+
     }
 }
 export default connect()(Home)
