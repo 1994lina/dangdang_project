2dc0ab1e09323b939bd129887fd8942e7d25f870
1994lina <31024781+1994lina@users.noreply.github.com>
2018-07-03 17:01:29 +0800

    Update Home.js

--- KETANG/src/routes/Home.js
+++ KETANG/src/routes/Home.js
@@ -1,8 +1,9 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import NavTop from '../component/NavTop'
 import { Carousel } from 'antd';
+import {Icon,Input} from 'antd';
+import '../static/css/home.less'
 
 class Home extends Component {
     constructor(props, context) {
@@ -14,27 +15,6 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <NavTop title={this.state.title} />
-                <main className='allBox'>
-                    <Carousel autoplay>
-                        <div><img src="http://img55.ddimg.cn/82310034954145_y.jpg" alt=""/></div>
-                        <div><img src="http://img63.ddimg.cn/upload_img/00702/B/1242x366_dl_20180629-1530512215.jpg" alt=""/></div>
-                        <div><img src="http://img61.ddimg.cn/upload_img/00670/qd/rhgu79kuo-1530528732.jpg" alt=""/></div>
-                        <div><img src="http://img60.ddimg.cn/upload_img/00609/mao/b0628banner1242366-1530524637.jpg" alt=""/></div>
-                    </Carousel>
-                </main>
-=======
-import React,{Component} from 'react';
-import {connect} from 'react-redux'
-import {Icon,Input} from 'antd';
-import '../static/css/home.less'
-
-class Home extends Component{
-    constructor(props,context){
-        super(props,context);
-    }
-    render(){
-        return <div>
                 <header className='headerNavBox'>
                     <div className='homeBox'>
                         <div className='baseBox'>
@@ -46,8 +26,14 @@ class Home extends Component{
                         </div>
                     </div>
                 </header>
-                <main className='allBox'>home</main>
->>>>>>> cd78e60032fb3d9677f76791458382af130e8fbd
+                <main className='allBox'>
+                    <Carousel autoplay>
+                        <div><img src="http://img55.ddimg.cn/82310034954145_y.jpg" alt=""/></div>
+                        <div><img src="http://img63.ddimg.cn/upload_img/00702/B/1242x366_dl_20180629-1530512215.jpg" alt=""/></div>
+                        <div><img src="http://img61.ddimg.cn/upload_img/00670/qd/rhgu79kuo-1530528732.jpg" alt=""/></div>
+                        <div><img src="http://img60.ddimg.cn/upload_img/00609/mao/b0628banner1242366-1530524637.jpg" alt=""/></div>
+                    </Carousel>
+                </main>
             </div>
     }
 }
